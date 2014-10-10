b32b77f145331e675a2103140099646b12166229
lionadi <adrian.simionescu82@gmail.com>
2014-10-10 11:20:08 +0300

    RandomBase Interfaces and Classes ported to Cocos2D-X

--- NatureOfCodeCocos2DX/Classes/Aurora/Random/RandomBase.h
+++ NatureOfCodeCocos2DX/Classes/Aurora/Random/RandomBase.h
@@ -15,7 +15,9 @@ using namespace Aurora::DataTypes;
 
 namespace Aurora {
     namespace Random {
-     
+		//-------------------------------------------------------------------------
+		// START = Random Base Interfaces
+		//-----------------------------------------------------------------------
         enum RandomNumberMode
         {
             Normal, Uniform, Gaussian, Perlin
@@ -26,52 +28,75 @@ namespace Aurora {
             protected:
                 RandomNumberMode RandomNumberModeValue;
             public:
+				PRandomBase();
+				virtual ~PRandomBase();
                 virtual void SetRandomNumberMode( RandomNumberMode randomNumberMode ) = 0;
         };
         
         
-        class PRandomBaseBasic : public PRandomBase
+        class PRandomBaseBasic : virtual public PRandomBase
         {
             protected:
                 Float Width;
                 Float Height;
             public:
+				PRandomBaseBasic();
+				virtual ~PRandomBaseBasic();
                 virtual void SetConstrainsRange( Float width, Float height) = 0;
                 virtual void DoCalculations() = 0;
                 virtual void Constrain() = 0;
-                virtual void Normal() = 0;
         };
         
-        
-        class PRandomBaseUniform : PRandomBaseBasic
+		class PRandomBaseNormal : virtual public PRandomBaseBasic
         {
             public:
+				PRandomBaseNormal();
+				virtual ~PRandomBaseNormal();
+                virtual void NormalCalculations() = 0;
+        };
+        
+        class PRandomBaseUniform : virtual public PRandomBaseBasic
+        {
+            public:
+				PRandomBaseUniform();
+				virtual ~PRandomBaseUniform();
                 virtual void UniformCalculations() = 0;
         };
         
-        class PRandomBaseGaussian : PRandomBaseBasic
+        class PRandomBaseGaussian : virtual public PRandomBaseBasic
         {
             public:
+				PRandomBaseGaussian();
+				virtual ~PRandomBaseGaussian();
                 virtual void GaussianCalculations() = 0;
         };
         
-        class PRandomBasePerlinNoise : PRandomBaseBasic
+        class PRandomBasePerlinNoise : virtual public PRandomBaseBasic
         {
             //TODO: IMPLEMENT AND USE PERLIN NOISE
             //var PerlinNoiseCalculator : PerlinNoise { get set };
             public:
+				PRandomBasePerlinNoise();
+				virtual ~PRandomBasePerlinNoise();
                 virtual void PerlinNoiseCalculations() = 0;
         };
         
-        class PRandomBaseComplete : PRandomBaseUniform, PRandomBaseGaussian, PRandomBasePerlinNoise
+        class PRandomBaseComplete : virtual public PRandomBaseUniform, virtual public PRandomBaseGaussian, virtual public PRandomBasePerlinNoise, virtual public PRandomBaseNormal
         {
-            
+		public:
+			PRandomBaseComplete();
+            virtual ~PRandomBaseComplete();
         };
+
+		//-------------------------------------------------------------------------
+		// END = Random Base Interfaces
+		//-----------------------------------------------------------------------
         
-        class RandomBase : public PRandomBaseBasic
+        class RandomBase : virtual public PRandomBaseBasic
         {
         public:
             RandomBase();
+			virtual ~RandomBase();
             
             void SetRandomNumberMode(RandomNumberMode randomNumberMode);
 
@@ -80,61 +105,60 @@ namespace Aurora {
             void Constrain();
             
             void DoCalculations();
-            
-            void Normal();
         };
 
-		class RandomBaseComplete : public RandomBase, public PRandomBaseComplete
+		class RandomBaseComplete : virtual public RandomBase, virtual public PRandomBaseComplete
 		{
-		public:
-			RandomBaseComplete();
+		private:
 			// TODO: PERLIN NOISE
 			//var PerlinNoiseCalculator : PerlinNoise = PerlinNoise(seed: 10);
-    /*
-			void DoCalculations()
-    {
-        
-        switch(self.RandomNumberModeValue)
-            {
-        case .Uniform:
-            self.UniformCalculations();
-            
-        case .Gaussian:
-            self.GaussianCalculations();
-        case .Perlin:
-            self.PerlinNoiseCalculations();
-        case .Normal:
-            self.Normal();
-        default:
-            self.UniformCalculations();
-        }
-        
-        self.Constrain();
-    }
-    
-    override func Constrain()
-    {
-        super.Constrain();
-    }
-    
-    override func Normal()
-    {
-        
-    }
-    
-    func UniformCalculations()
-    {
-        
-    }
-    
-    func GaussianCalculations()
-    {
-        
-    }
-    
-    func PerlinNoiseCalculations()
-    {
-    }*/
+		public:
+			RandomBaseComplete();
+			virtual ~RandomBaseComplete();
+			void DoCalculations();
+			void Constrain();
+			void NormalCalculations();
+			void UniformCalculations();
+			void GaussianCalculations();
+			void PerlinNoiseCalculations();
+		};
+
+		class RandomBasePerlinNoise : virtual public RandomBase, virtual public PRandomBasePerlinNoise
+		{
+		public:
+			// TODO: PERLIN NOISE
+			//var PerlinNoiseCalculator : PerlinNoise = PerlinNoise(seed: 10);
+			RandomBasePerlinNoise();
+			virtual ~RandomBasePerlinNoise();
+			void DoCalculations();
+			void PerlinNoiseCalculations();
+		};
+
+		class RandomBaseGaussian : virtual public RandomBase, virtual public PRandomBaseGaussian
+		{
+		public:
+			RandomBaseGaussian();
+			virtual ~RandomBaseGaussian();
+			void DoCalculations();
+			void GaussianCalculations();
+		};
+
+		class RandomBaseUniform : virtual public RandomBase, virtual public PRandomBaseUniform
+		{
+		public:
+			RandomBaseUniform();
+			virtual ~RandomBaseUniform();
+			void DoCalculations();
+			void UniformCalculations();
+		};
+
+		class RandomBaseNormal : virtual public RandomBase, virtual public PRandomBaseNormal
+		{
+		public:
+			RandomBaseNormal();
+			virtual ~RandomBaseNormal();
+			void DoCalculations();
+			void NormalCalculations();
 		};
         
     }; // END OF NAMESPACE Random
